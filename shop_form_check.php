4235052825362fac63016486b12cf49f5803df5e
noriya <noriya.saeki@gmail.com>
2019-08-26 18:27:59 +0900

    注文確認作成

--- shop_form_check.php
+++ shop_form_check.php
@@ -17,28 +17,58 @@
         $address = $post['address'];
         $tel = $post['tel'];
 
+        $okflg = true;
+
         if ($onamae == '') {
             print 'お名前が入力されていません<br><br>';
+            $okflg = false;
+        } else {
+            echo 'お名前<br>'.$onamae.'<br><br>';
         }
         if (preg_match('/^[\w\.\-]+\@[\w\-\.]+\.([a-zA-Z0-9]{2,4})$/', $email) == 0) {
-            echo 'メールアドレスを性格に入力して下さい（例：test@example.com）<br><br>';
+            echo 'メールアドレスを正確に入力して下さい（例：test@example.com）<br><br>';
+            $okflg = false;
+        } else {
+            echo 'メールアドレス<br>'.$email.'<br><br>';
         }
         if (preg_match('/\A[0-9]+\z/', $postal1) == 0) {
             echo '郵便番号は半角数字で入力してください<br><br>';
-        }
-        if (preg_match('/\A[0-9]+\z/', $postal2) == 0) {
+            $okflg = false;
+        } else if (preg_match('/\A[0-9]+\z/', $postal2) == 0) {
             echo '郵便番号は半角数字で入力してください<br><br>';
+            $okflg = false;
+        } else {
+            echo '郵便番号<br>'.$postal1.'-'.$postal2.'<br><br>';
         }
         if ($address == '') {
             echo '住所が入力されていません。<br><br>';
+            $okflg = false;
+        } else {
+            echo '住所<br>'.$address.'<br><br>';
         }
         if (preg_match('/\A\d{2,5}-?\d{2,5}-?\d{4,5}\z/', $tel) == 0) {
-            echo '電話番号を性格に入力してください。<br><br>';
+            echo '電話番号を正確に入力してください。<br><br>';
+            $okflg = false;
+        } else {
+            echo '電話番号<br>'.$tel.'<br><br>';
         }
 
-        echo '<form method="post" action="shop_form_done.php">';
-            echo '<input >';
-        echo '</form>';
+        if ($okflg == true) {
+            echo '<form method="post" action="shop_form_done.php">';
+            echo '<input type="hidden" name="onamae" value="'.$onamae.'">';
+            echo '<input type="hidden" name="email" value="'.$email.'">';
+            echo '<input type="hidden" name="postal1" value="'.$postal1.'">';
+            echo '<input type="hidden" name="postal2" value="'.$postal2.'">';
+            echo '<input type="hidden" name="address" value="'.$address.'">';
+            echo '<input type="hidden" name="tel" value="'.$tel.'">';
+            echo '<input type="button" onclick="history.back()" value="戻る">';
+            echo '<input type="submit" value="OK">';
+            echo '</form>';
+        } else {
+            echo '<form>';
+            echo '<input type="button" onclick="history.back()" value="戻る">';
+            echo '</form>';
+        }
         ?>
     </body>
 </html>
