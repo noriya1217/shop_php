08bf7be22f24187a5c0bd7bf715beb355e2c6712
noriya <noriya.saeki@gmail.com>
2019-08-27 14:16:47 +0900

    注文確定作成

--- shop_form_done.php
+++ shop_form_done.php
@@ -40,6 +40,12 @@
             $cart = $_SESSION['cart'];
             $kazu = $_SESSION['kazu'];
             $max = count($cart);
+            
+            $_SESSION = array();
+            if (isset($_COOKIE[session_name()]) == true) {
+                setcookie(session_name(), '', time()-42000, '/');
+            }
+            session_destroy();
 
             $dsn = 'mysql:dbname=shop;host=localhost;carset=utf8';
             $user = 'root';
@@ -57,6 +63,7 @@
 
                 $name = $rec['name'];
                 $price = $rec['price'];
+                $kakaku[] = $price;
                 $suryo = $kazu[$i];
                 $shokei = $price * $suryo;
 
@@ -65,6 +72,43 @@
                 $honbun .= $suryo.'個 = ';
                 $honbun .= $shokei."円\n";
             }
+
+            $sql = 'LOCK TABLES dat_sales WRITE, dat_sales_product WRITE';
+            $stmt = $dbh -> prepare($sql);
+            $stmt -> execute();
+
+            $sql = 'INSERT INTO dat_sales (code_member, name, email, postal1, postal2, address, tel) VALUES (?, ?, ?, ?, ?, ?, ?)';
+            $stmt = $dbh -> prepare($sql);
+            $data = array();
+            $data[] = 0;
+            $data[] = $onamae;
+            $data[] = $email;
+            $data[] = $postal1;
+            $data[] = $postal2;
+            $data[] = $address;
+            $data[] = $tel;
+            $stmt -> execute($data);
+
+            $sql = 'SELECT LAST_INSERT_ID()';
+            $stmt = $dbh -> prepare($sql);
+            $stmt -> execute();
+            $rec = $stmt -> fetch(PDO::FETCH_ASSOC);
+            $lastcode = $rec['LAST_INSERT_ID()'];
+
+            for ($i = 0; $i < $max; $i ++) {
+                $sql = 'INSERT INTO dat_sales_product (code_sales, code_product, price, quantity) VALUES (?, ?, ?, ?)';
+                $stmt = $dbh -> prepare($sql);
+                $data = array();
+                $data[] = $lastcode;
+                $data[] = $cart[$i];
+                $data[] = $kakaku[$i];
+                $data[] = $kazu[$i];
+                $stmt -> execute($data);
+            }
+
+            $sql = 'UNLOCK TABLES';
+            $stmt = $dbh -> prepare($sql);
+            $stmt -> execute();
 
             $dbh = null;
 
@@ -105,5 +149,8 @@
             exit();
         }
         ?>
+
+        <br>
+        <a href="shop_list.php">商品画面へ</a>
     </body>
 </html>
